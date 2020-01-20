b02a677d019b1eec3981d9c3687c713eb334e268
Fil Zembowicz <fil@formsort.com>
2020-01-20 12:43:17 -0500

    Add test routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,28 @@
+import time
 from app import app
+from uuid import uuid4
+from flask import jsonify
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route("/infinite-loop")
+def infinite_loop():
+    a = 0
+    while True:
+        a += random.random() - 0.5
+
+
+@app.route("/sleep/<int:seconds>")
+def sleep(seconds):
+    SLEEP_PERIOD_S = 5
+    request_id = uuid4()
+    for i in range(int(seconds / SLEEP_PERIOD_S)):
+        time.sleep(SLEEP_PERIOD_S)
+        print(
+            f"[request {request_id}] slept {(i + 1) * SLEEP_PERIOD_S} seconds, zzzz"
+        )
+    return jsonify({"result": f"[request {request_id}] slept for {seconds} seconds, done"})
+
